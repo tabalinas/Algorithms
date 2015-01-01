ff91dd17873eb078176ff1b7912d908e69b068aa
Artem Tabalin <tabalinas@gmail.com>
2015-01-01 19:31:30 +0400

    DP: Money change problem Problem: How can a given amount of money be made with the least number of coins of given denominations

---
+++ DynamicProgramming/MoneyChange.Test/MoneyChangeTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyChange.Test {
+
+    [TestClass]
+    public class MoneyChangeTests {
+
+        [TestMethod]
+        public void MoneyChangeTest() {
+            // arrange
+            int[] coins = new int[] { 1, 5, 7 };
+            int targetSum = 11;
+            var changeCombiner = new ChangeCombiner(coins);
+
+            // act
+            int result = changeCombiner.MinCoinsAmountForSum(targetSum);
+
+            // assert
+            Assert.AreEqual<int>(3, result, "Minimal amount of coins should be equal to 3 (5 + 5 + 1 = 11)");
+        }
+
+    }
+}
---
+++ DynamicProgramming/MoneyChange/ChangeCombiner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyChange
+{
+    public class ChangeCombiner
+    {
+        private readonly int[] _coins;
+
+        public int[] Coins {
+            get { return _coins; }
+        }
+
+        public ChangeCombiner(int[] coins) {
+            _coins = coins;
+        }
+
+        public int MinCoinsAmountForSum(int sum) {
+            int[] dp = Enumerable.Repeat<int>(int.MaxValue, sum+1).ToArray();
+            dp[0] = 0;
+
+            for(int s = 1; s <= sum; s++) {
+                foreach(int coin in Coins) {
+                    if(s < coin)
+                        break;
+
+                    dp[s] = Math.Min(dp[s-coin] + 1, dp[s]);
+                }
+            }
+
+            return dp[sum];
+        }
+    }
+}
