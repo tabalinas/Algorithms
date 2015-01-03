337ef43f111d0dd6d2f2327db7c7527f93ff029b
Artem Tabalin <tabalinas@gmail.com>
2015-01-03 20:41:03 +0300

    DP: Max Value Contiguous Subsequence Problem: Given a sequence of n numbers A(1) ... A(n), determine a contiguous subsequence A(i) ... A(j) for which the sum of elements in the subsequence is maximized.

---
+++ DynamicProgramming/MaxValueContiguousSubsequence.Test/MaxValueContiguousSubsequenceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaxValueContiguousSubsequence.Test {
+
+    [TestClass]
+    public class MaxValueContiguousSubsequenceTests {
+
+        [TestMethod]
+        public void FindMaxValueContiguousSubsequenceTest() {
+            // arrange
+            int[] array = new int[] { -1, 11, -5, 13, -3, 2 };
+
+            // act
+            int[] result = new SequenceHelper().FindMaxValueContiguousSubsequence(array);
+
+            // assert
+            CollectionAssert.AreEqual(new int[] { 11, -5, 13 }, result);
+        }
+
+    }
+
+}
---
+++ DynamicProgramming/MaxValueContiguousSubsequence/SequenceHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxValueContiguousSubsequence {
+
+    /// <summary>
+    /// Given a sequence of n numbers A(1) ... A(n), determine a contiguous subsequence A(i) ... A(j) for which the sum of elements in the subsequence is maximized.
+    /// </summary>
+    public class SequenceHelper {
+
+        public int[] FindMaxValueContiguousSubsequence(int[] array) {
+            int sum = array[0];
+            int maxSum = sum;
+            int maxStart = 0;
+            int maxEnd = 0;
+
+            for(int i = 1, j = 0; i < array.Length; i++) {
+                if(sum + array[i] > array[i]) {
+                    sum = sum + array[i];
+                } else {
+                    sum = array[i];
+                    j = i;
+                }
+
+                if(sum > maxSum) {
+                    maxEnd = i;
+                    maxStart = j;
+                    maxSum = sum;
+                }
+            }
+
+            return array.Skip(maxStart).Take(maxEnd - maxStart + 1).ToArray();
+        }
+
+    }
+
+}
