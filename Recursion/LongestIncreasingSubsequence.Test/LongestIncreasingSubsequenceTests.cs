f5b94b17a50b4d7c2a02e4ace33b72e2f3f70c87
Artem Tabalin <tabalinas@gmail.com>
2015-01-04 11:02:34 +0300

    Recursion: Longest Increasing Subsequence Problem: Given a sequence of n real numbers A(1) ... A(n), determine a length of a subsequence (not necessarily contiguous) of maximum length in which the values in the subsequence form a strictly increasing sequence.

---
+++ Recursion/LongestIncreasingSubsequence.Test/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LongestIncreasingSubsequence.Test {
+
+    [TestClass]
+    public class LongestIncreasingSubsequenceTests {
+
+        [TestMethod]
+        public void FindLongestIncreasingSubsequenceRecursive() {
+            // arrange
+            int[] array = new int[] { 100, 1, 11, 2, 12, 3, 4 };
+
+            // act
+            int result = new SequenceHelper().FindLongestIncreasingSubsequence(array);
+
+            // assert
+            Assert.AreEqual(4, result, "The length of the longest increasing subsequence should be four since it is { 1, 2, 3, 4 }");
+        }
+
+    }
+
+}
---
+++ Recursion/LongestIncreasingSubsequence/SequenceHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestIncreasingSubsequence {
+
+    /// <summary>
+    /// Given a sequence of n real numbers A(1) ... A(n), determine a subsequence (not necessarily contiguous) of maximum length in which the values in the subsequence form a strictly increasing sequence.
+    /// </summary>
+    public class SequenceHelper {
+
+        public int FindLongestIncreasingSubsequence(int[] array) {
+            return FindLongestIncreasingSubsequence(array, 0, int.MinValue);
+        }
+
+        private int FindLongestIncreasingSubsequence(int[] array, int i, int prevValue) {
+            if(i >= array.Length)
+                return 0;
+
+            int max = FindLongestIncreasingSubsequence(array, i + 1, prevValue);
+            int length = int.MinValue;
+
+            if(array[i] > prevValue) {
+                length = FindLongestIncreasingSubsequence(array, i + 1, array[i]) + 1;
+            }
+
+            return Math.Max(max, length);
+        }
+
+    }
+
+}
+
