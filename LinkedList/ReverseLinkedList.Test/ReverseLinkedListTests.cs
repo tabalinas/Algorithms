79814813bdce0be854f141aaac282236476a736c
Artem Tabalin <tabalinas@gmail.com>
2015-01-05 23:42:26 +0300

    LinkedList: Reverse linked list

---
+++ LinkedList/ReverseLinkedList.Test/ReverseLinkedListTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseLinkedList.Test {
+
+    [TestClass]
+    public class ReverseLinkedListTests {
+
+        [TestMethod]
+        public void ReverseLinkedListTest() {
+            // arrange
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            LinkedList<int> list = new LinkedList<int>(array);
+
+            // act
+            list.Reverse();
+
+            // assert
+            Assert.AreEqual("5->4->3->2->1", list.ToString());
+        }
+
+        [TestMethod]
+        public void ReverseSingleElementLinkedListTest() {
+            // arrange
+            int[] array = new int[] { 1 };
+            LinkedList<int> list = new LinkedList<int>(array);
+
+            // act
+            list.Reverse();
+
+            // assert
+            Assert.AreEqual("1", list.ToString());
+        }
+
+    }
+
+}
---
+++ LinkedList/ReverseLinkedList/LinkedListEx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace ReverseLinkedList {
+
+    public static class LinkedListEx {
+
+        public static void Reverse<T>(this LinkedList<T> list) {
+            if(list.Head == null)
+                return;
+
+            list.Reverse(list.Head);
+        }
+
+        private static void Reverse<T>(this LinkedList<T> list, LinkedList<T>.Node node) {
+            var next = node.Next;
+
+            if(next == null) {
+                list.Head = node;
+            } else {
+                list.Reverse(next);
+                next.Next = node;
+            }
+
+            node.Next = null;
+        }
+
+    }
+
+}
