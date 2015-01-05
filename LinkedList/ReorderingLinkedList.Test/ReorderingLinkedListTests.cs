5696fd7509981ca57ca24e08da6ee69a9f9cb0a3
Artem Tabalin <tabalinas@gmail.com>
2015-01-06 01:35:03 +0300

    LinkedList: Reorder linked list elements as (1) -> (N) -> (2) -> (N-1) ... -> (i) -> (N-i-1) e.g. 1->2->3->4->5 becomes 1->5->2->4->3

---
+++ LinkedList/ReorderingLinkedList.Test/ReorderingLinkedListTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReorderingLinkedList.Test {
+
+    [TestClass]
+    public class ReorderingLinkedListTests {
+
+        [TestMethod]
+        public void ReorderLinkedListWithOddAmountOfElementsTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+            
+            // act
+            list.Reorder();
+
+            // assert
+            Assert.AreEqual("1->7->2->6->3->5->4", list.ToString());
+        }
+
+        [TestMethod]
+        public void ReorderLinkedListWithEvenAmountOfElementsTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            // act
+            list.Reorder();
+
+            // assert
+            Assert.AreEqual("1->6->2->5->3->4", list.ToString());
+        }
+
+    }
+
+}
---
+++ LinkedList/ReorderingLinkedList/LinkedListEx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace ReorderingLinkedList {
+
+    /// <summary>
+    /// Reorder linked list elements as (1) -> (N) -> (2) -> (N-1) ... -> (i) -> (N-i-1) e.g. 1->2->3->4->5 becomes 1->5->2->4->3
+    /// </summary>
+    public static class LinkedListEx {
+
+        public static void Reorder<T>(this LinkedList<T> list) {
+            var firstHalf = list.Head;
+            
+            var middle = list.Middle();
+            CutOfFirstHalf(list, middle);
+            Reverse(list, middle);
+
+            var secondHalf = list.Head;
+
+            Merge(list, firstHalf, secondHalf);
+        }
+
+        public static LinkedList<T>.Node Middle<T>(this LinkedList<T> list) {
+            var slow = list.Head;
+            var fast = list.Head;
+
+            while(fast != null && fast.Next != null) {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            return slow;
+        }
+
+        private static void CutOfFirstHalf<T>(LinkedList<T> list, LinkedList<T>.Node middle) {
+            var current = list.Head;
+            while(current.Next != middle) {
+                current = current.Next;
+            }
+            current.Next = null;
+        }
+
+        public static void Reverse<T>(LinkedList<T> list, LinkedList<T>.Node node) {
+            var next = node.Next;
+
+            if(next == null) {
+                list.Head = node;
+            } else {
+                Reverse(list, next);
+                next.Next = node;
+            }
+
+            node.Next = null;
+        }
+
+        private static void Merge<T>(LinkedList<T> list, LinkedList<T>.Node firstHalf, LinkedList<T>.Node secondHalf) {
+            var first = firstHalf;
+            var second = secondHalf;
+            list.Head = new LinkedList<T>.Node(default(T));
+            var current = list.Head;
+
+            while(first != null || second != null) {
+                if(first != null) {
+                    current.Next = first;
+                    first = first.Next;
+                    current = current.Next;
+                }
+
+                if(second != null) {
+                    current.Next = second;
+                    second = second.Next;
+                    current = current.Next;
+                }
+            }
+
+            list.Head = list.Head.Next;
+        }
+
+    }
+
+}
