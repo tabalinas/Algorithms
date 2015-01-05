3d61dfea318b557324efd64c1cbff3d3ea528d21
Artem Tabalin <tabalinas@gmail.com>
2015-01-05 03:32:39 +0300

    LinkedList: Data Structure

---
+++ LinkedList/LinkedList.Test/LinkedListTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedList.Test {
+
+    [TestClass]
+    public class LinkedListTests {
+
+        [TestMethod]
+        public void CreateLinkedListTest() {
+            // arrange
+            int[] array = new int[] { 1, 2, 3 };
+
+            // act
+            LinkedList<int> list = new LinkedList<int>(array);
+
+            // assert
+            Assert.AreEqual("1->2->3", list.ToString());
+        }
+
+        [TestMethod]
+        public void InsertItemInLinkedListTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3 });
+            int itemToInsert = 4;
+
+            // act
+            list.Insert(itemToInsert);
+
+            // assert
+            Assert.AreEqual("1->2->3->4", list.ToString());
+        }
+
+        [TestMethod]
+        public void InsertMultipleItemsInLinkedListTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3 });
+            int[] itemsToInsert = new int[] { 4, 5, 6 };
+
+            // act
+            list.Insert(itemsToInsert);
+
+            // assert
+            Assert.AreEqual("1->2->3->4->5->6", list.ToString());
+        }
+
+        [TestMethod]
+        public void RemoveItemFromLinkedListTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3, 7, 4, 5, 6 });
+            int itemToRemove = 7;
+
+            // act
+            bool result = list.Remove(itemToRemove);
+
+            // assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual("1->2->3->4->5->6", list.ToString());
+        }
+
+        [TestMethod]
+        public void RemoveFirstItemFromLinkedListTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 0, 1, 2, 3, 4, 5, 6 });
+            int itemToRemove = 0;
+
+            // act
+            bool result = list.Remove(itemToRemove);
+
+            // assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual("1->2->3->4->5->6", list.ToString());
+        }
+
+        [TestMethod]
+        public void RemoveNonExistingItemFromLinkedListTest() {
+            // arrange
+            LinkedList<int> list = new LinkedList<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+            int itemToRemove = 100;
+
+            // act
+            bool result = list.Remove(itemToRemove);
+
+            // assert
+            Assert.AreEqual(false, result);
+            Assert.AreEqual("1->2->3->4->5->6", list.ToString());
+        }
+    }
+}
---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList {
+
+    public class LinkedList<T> {
+
+        private class Node {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T value) {
+                Value = value;
+            }
+        }
+
+        private Node Head { get; set; }
+
+        public LinkedList(T[] items) {
+            Insert(items);
+        }
+
+        private void Insert(Node node) {
+            if(Head == null) {
+                Head = node;
+                return;
+            }
+
+            var current = Head;
+            while(current.Next != null) {
+                current = current.Next;
+            }
+            current.Next = node;
+        }
+
+        public void Insert(T item) {
+            Insert(new Node(item));
+        }
+
+        public void Insert(T[] items) {
+            if(items == null || items.Length == 0)
+                return;
+
+            var node = new Node(items[0]);
+            Insert(node);
+
+            for(int i = 1; i < items.Length; i++) {
+                node.Next = new Node(items[i]);
+                node = node.Next;
+            }
+        }
+
+        public bool Remove(T value) {
+            if(Head == null)
+                return false;
+
+            if(Head.Value.Equals(value)) {
+                Head = Head.Next;
+                return true;
+            }
+
+            for(Node prev = Head, current = Head.Next; current != null; prev = current, current = current.Next) {
+                if(current.Value.Equals(value)) {
+                    prev.Next = current.Next;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override string ToString() {
+            if(Head == null)
+                return "";
+
+            var result = new StringBuilder(Head.Value.ToString());
+
+            for(var current = Head.Next; current != null; current = current.Next) {
+                result.Append("->" + current.Value.ToString());
+            }
+            
+            return result.ToString();
+        }
+    }
+}
