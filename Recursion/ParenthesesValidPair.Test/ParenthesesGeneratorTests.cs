36abe7487d81dc58693c28bc754f78c086a8d34f
Artem Tabalin <tabalinas@gmail.com>
2015-01-06 11:49:50 +0300

    Recursion: Parenthesis valid pairs generation Problem: Generate all valid (properly opened and closed) combinations of n-pairs of parentheses

---
+++ Recursion/ParenthesesValidPair.Test/ParenthesesGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParenthesesValidPairs;
+
+namespace ParenthesesValidPair.Test {
+
+    [TestClass]
+    public class ParenthesesGeneratorTests {
+
+        [TestMethod]
+        public void GenerateParenthesesTest() {
+            // arrange
+            var parenthesesGenerator = new ParenthesesGenerator();
+            int amountToGenerate = 3;
+            HashSet<string> expectedResult = new HashSet<string>() { "((()))", "(()())", "(())()", "()(())", "()()()" };
+
+            // act
+            HashSet<string> result = parenthesesGenerator.Generate(amountToGenerate);
+
+            // assert
+            CollectionAssert.AreEqual(expectedResult.ToList(), result.ToList());
+        }
+
+    }
+}
---
+++ Recursion/ParenthesesValidPairs/ParenthesesGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParenthesesValidPairs {
+
+    public class ParenthesesGenerator {
+
+        private HashSet<string> _result;
+
+        public HashSet<string> Generate(int amount) {
+            _result = new HashSet<string>();
+
+            Generate("", amount, amount);
+
+            return _result;
+        }
+
+        private void Generate(string parentheses, int openedAmount, int closedAmount) {
+            if(openedAmount == 0 && closedAmount == 0) {
+                _result.Add(parentheses);
+                return;
+            }
+
+            if(openedAmount > 0) {
+                Generate(parentheses + "(", openedAmount - 1, closedAmount);
+            }
+
+            if(closedAmount > openedAmount) {
+                Generate(parentheses + ")", openedAmount, closedAmount - 1);
+            }
+        }
+    }
+
+}
