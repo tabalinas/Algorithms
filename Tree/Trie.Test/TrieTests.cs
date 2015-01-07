9d86fcea7cc29a4e13c963d2a9c56502f8bb0ea6
Artem Tabalin <tabalinas@gmail.com>
2015-01-07 23:16:01 +0300

    Tree: Trie implementation

---
+++ Tree/Trie.Test/TrieTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trie.Test {
+
+    [TestClass]
+    public class TrieTests {
+
+        [TestMethod]
+        public void AddWordsToTrieTest() {
+            // arrange
+            var trie = new Trie();
+
+            // act
+            trie.AddWord("apple");
+            trie.AddWord("baloon");
+            trie.AddWord("cat");
+            trie.AddWord("car");
+            trie.AddWord("carpet");
+
+            // assert
+            Assert.AreEqual("apple, baloon, car, carpet, cat", trie.ToString());
+        }
+
+        [TestMethod]
+        public void FindWordsInTrieTest() {
+            // arrange
+            var trie = new Trie();
+            trie.AddWord("apple");
+            trie.AddWord("car");
+            trie.AddWord("carbon");
+            trie.AddWord("carpet");
+            trie.AddWord("call");
+
+            // act
+            IList<string> result = trie.FindWords("car");
+
+            // assert
+            Assert.AreEqual("car, carbon, carpet", String.Join(", ", result.ToArray()));
+        }
+
+    }
+
+}
---
+++ Tree/Trie/Trie.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trie {
+
+    public class Trie {
+
+        private const int ALPHABET_SIZE = 26;
+
+        public class Node {
+
+            public string Value { get; set; }
+            public Node[] Children { get; set; }
+            public bool IsLeaf { get; set; }
+            public bool IsWord { get; set; }
+
+            public Node(string value) {
+                Value = value;
+                Children = new Node[ALPHABET_SIZE];
+                IsLeaf = true;
+            }
+
+        }
+
+        private Node Root;
+
+        public Trie() {
+            Root = new Node("");
+        }
+
+        private int Index(char ch) {
+            return ch - 'a';
+        }
+
+        public void AddWord(string word) {
+            word = word.ToLower();
+            var node = Root;
+
+            for(int i = 0; i < word.Length; i++) {
+                char ch = word[i];
+                int index = Index(ch);
+                var child = node.Children[index];
+
+                if(child == null) {
+                    child = new Node(ch.ToString());
+                    node.Children[index] = child;
+                    node.IsLeaf = false;
+                }
+                
+                node = child;
+            }
+
+            node.IsWord = true;
+        }
+
+        public IList<string> FindWords(string prefix) {
+            prefix = prefix.ToLower();
+            var node = Root;
+
+            for(int i = 0; i < prefix.Length; i++) {
+                char ch = prefix[i];
+                var child = node.Children[Index(ch)];
+
+                if(child == null)
+                    return new List<string>();
+
+                node = child;
+            }
+
+            return GetWords(node, prefix.Substring(0, prefix.Length - 1));
+        }
+
+        private IList<string> GetWords(Node node, string prefix) {
+            var result = new List<string>();
+            GetWords(node, prefix, result);
+            return result;
+        } 
+
+        private void GetWords(Node node, string word, IList<string> all) {
+            word = word + node.Value;
+
+            if(node.IsWord) {
+                all.Add(word);
+            }
+
+            if(node.IsLeaf)
+                return;
+
+            foreach(Node child in node.Children) {
+                if(child == null)
+                    continue;
+
+                GetWords(child, word, all);
+            }
+        }
+
+        public override string ToString() {
+            var result = GetWords(Root, "");
+            return String.Join(", ", result.ToArray());
+        }
+
+    }
+
+}
