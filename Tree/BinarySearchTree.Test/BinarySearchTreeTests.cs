2c2b48bd58aa5f221b92a131634fb4fe3ce80193
Artem Tabalin <tabalinas@gmail.com>
2015-01-07 03:51:33 +0300

    Tree: Binary search tree construction and traversals (in-, pre-, post-order)

---
+++ Tree/BinarySearchTree.Test/BinarySearchTreeTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySearchTree.Test {
+
+    [TestClass]
+    public class BinarySearchTreeTests {
+
+        [TestMethod]
+        public void AddElementsToBinarySearchTreeTest() {
+            // arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+ 
+            // act
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(1);
+            tree.Add(9);
+            tree.Add(11);
+            tree.Add(19);
+
+            // assert
+            Assert.AreEqual("(((1) <- 5 -> (9)) <- 10 -> ((11) <- 15 -> (19)))", tree.ToString());
+        }
+
+        [TestMethod]
+        public void FindElementInBinarySearchTreeTest() {
+            // arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+
+            // act
+            bool has15 = tree.Contains(15);
+            bool has30 = tree.Contains(30);
+
+            // assert
+            Assert.IsTrue(has15);
+            Assert.IsFalse(has30);
+        }
+
+        [TestMethod]
+        public void InOrderTraversalTest() {
+            // arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(1);
+            tree.Add(9);
+
+            // act
+            StringBuilder result = new StringBuilder();
+            tree.InOrder(val => result.Append(val + " "));
+
+            // assert
+            Assert.AreEqual("1 5 9 10 15 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PreOrderTraversalTest() {
+            // arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(1);
+            tree.Add(9);
+
+            // act
+            StringBuilder result = new StringBuilder();
+            tree.PreOrder(val => result.Append(val + " "));
+
+            // assert
+            Assert.AreEqual("10 5 1 9 15 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PostOrderTraversalTest() {
+            // arrange
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(1);
+            tree.Add(9);
+
+            // act
+            StringBuilder result = new StringBuilder();
+            tree.PostOrder(val => result.Append(val + " "));
+
+            // assert
+            Assert.AreEqual("1 9 5 15 10 ", result.ToString());
+        }
+
+    }
+}
---
+++ Tree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree {
+
+    public class BinarySearchTree<T> where T: IComparable {
+
+        public class Node {
+
+            public Node Right { get; set; }
+            public Node Left { get; set; }
+            public T Value { get; set; }
+
+            public Node(T value) {
+                Value = value;
+            }
+
+        }
+
+        public Node Root { get; set; }
+
+        public void Add(T value) {
+            Root = Add(Root, new Node(value));
+        }
+
+        private Node Add(Node parent, Node node) {
+            if(parent == null)
+                return node;
+
+            switch(node.Value.CompareTo(parent.Value)) {
+                case 1:
+                    parent.Right = Add(parent.Right, node);
+                    break;
+                case -1:
+                    parent.Left = Add(parent.Left, node);
+                    break;
+                default:
+                    throw new Exception("Binary search tree should not contain duplicates!");
+            }
+
+            return parent;
+        }
+
+        public bool Contains(T value) {
+            return Contains(Root, value);
+        }
+
+        private bool Contains(Node node, T value) {
+            if(node == null)
+                return false;
+
+            switch(value.CompareTo(node.Value)) {
+                case 1:
+                    return Contains(node.Right, value);    
+                case -1:
+                    return Contains(node.Left, value);
+                default:
+                    return true;
+            }
+        }
+
+        public void InOrder(Action<T> handler) {
+            InOrder(Root, handler);
+        }
+
+        private void InOrder(Node node, Action<T> handler) {
+            if(node == null)
+                return;
+
+            InOrder(node.Left, handler);
+            handler(node.Value);
+            InOrder(node.Right, handler);
+        }
+
+        public void PreOrder(Action<T> handler) {
+            PreOrder(Root, handler);
+        }
+
+        private void PreOrder(Node node, Action<T> handler) {
+            if(node == null)
+                return;
+
+            handler(node.Value);
+            PreOrder(node.Left, handler);
+            PreOrder(node.Right, handler);
+        }
+
+        public void PostOrder(Action<T> handler) {
+            PostOrder(Root, handler);
+        }
+
+        private void PostOrder(Node node, Action<T> handler) {
+            if(node == null)
+                return;
+
+            PostOrder(node.Left, handler);
+            PostOrder(node.Right, handler);
+            handler(node.Value);
+        }
+
+        public override string ToString() {
+            return Stringify(Root);
+        }
+
+        private string Stringify(Node node) {
+            if(node == null)
+                return "";
+
+            string leftSubtree = Stringify(node.Left);
+            string rightSubtree = Stringify(node.Right);
+
+            return String.Format("({0}{1}{2})", 
+                String.IsNullOrEmpty(leftSubtree) ? "" : leftSubtree + " <- ",
+                node.Value,
+                String.IsNullOrEmpty(rightSubtree) ? "" : " -> " + rightSubtree);
+        }
+
+    }
+}
