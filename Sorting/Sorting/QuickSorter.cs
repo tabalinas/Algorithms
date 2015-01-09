581a572997d6efd90d12b70a37de4c2155968d63
Artem Tabalin <tabalinas@gmail.com>
2015-01-09 19:02:42 +0300

    Sorting: Quick sort

---
+++ Sorting/Sorting/QuickSorter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting {
+
+    public class QuickSorter<T>: Sorter<T> where T: IComparable {
+
+        public QuickSorter(T[] array)
+            : base(array) { }
+
+        public override void Sort() {
+            QuickSort(0, Array.Length - 1);
+        }
+
+        private void QuickSort(int start, int end) {
+            if(end <= start)
+                return;
+
+            int pivot = new Random().Next(end - start + 1) + start;
+
+            Swap(start, pivot);
+
+            int current = start;
+            for(int i = start + 1; i <= end; i++) {
+                if(Array[start].CompareTo(Array[i]) > 0) {
+                    Swap(++current, i);
+                }
+            }
+
+            Swap(start, current);
+
+            QuickSort(start, current - 1);
+            QuickSort(current + 1, end);
+        }
+
+    }
+
+}
--- Sorting/Sotring.Test/SortingTests.cs
+++ Sorting/Sotring.Test/SortingTests.cs
@@ -43,6 +43,20 @@ namespace Sotring.Test {
             CollectionAssert.AreEqual(new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 }, array);
         }
 
+        [TestMethod]
+        public void QuickSortingTest() {
+            // arrage
+            var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
+
+            array = new int[] { 2, -86, -50, -4, 43, 62, 38, 87, 7, 68 };
+
+            // act
+            new QuickSorter<int>(array).Sort();
+
+            // assert
+            CollectionAssert.AreEqual(new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 }, array);
+        }
+
     }
 
 }
