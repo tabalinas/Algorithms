f984821094eb2f0a57408123a0f128def8278bc0
Artem Tabalin <tabalinas@gmail.com>
2015-01-09 18:12:54 +0300

    Sorting: Bubble sort

---
+++ Sorting/Sorting/BubbleSorter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting {
+
+    public class BubbleSorter<T>: Sorter<T> where T: IComparable {
+
+        public BubbleSorter(T[] array)
+            : base(array) { }
+
+        public override void Sort() {
+            bool swapped;
+
+            for(int i = 0; i < Array.Length; i++) {
+                swapped = false;
+
+                for(int j = Array.Length - 1; j > i; j--) {
+                    if(Array[j - 1].CompareTo(Array[j]) > 0) {
+                        Swap(j - 1, j);
+                        swapped = true;
+                    }
+                }
+
+                if(!swapped)
+                    break;
+            }
+        }
+
+    }
+
+}
--- Sorting/Sotring.Test/SortingTests.cs
+++ Sorting/Sotring.Test/SortingTests.cs
@@ -8,6 +8,18 @@ namespace Sotring.Test {
     public class SortingTests {
 
         [TestMethod]
+        public void BubbleSortingTest() {
+            // arrage
+            var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
+
+            // act
+            new BubbleSorter<int>(array).Sort();
+
+            // assert
+            CollectionAssert.AreEqual(new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 }, array);
+        }
+
+        [TestMethod]
         public void InsertionSortingTest() {
             // arrage
             var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
