3584e9ecd0f65021ae8b50b0d13f8ef2ddc3524d
Artem Tabalin <tabalinas@gmail.com>
2015-01-10 18:07:20 +0300

    Arrays: Quick select Problem: Find the Nth element in an unordered array

---
+++ Arrays/QuickSelect.Test/QuickSelectTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuickSelect.Test {
+
+    [TestClass]
+    public class QuickSelectTests {
+
+        [TestMethod]
+        public void QuickSelectTest() {
+            // arrange
+            var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
+
+            // act
+            var result = new QuickSelector<int>(array).FindNth(5);
+
+            // assert
+            Assert.AreEqual(38, result, "item 38 is at index 5 if arrays would be sorted");
+        }
+
+        [TestMethod]
+        public void QuickSelectForLastItemTest() {
+            // arrange
+            var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
+
+            // act
+            var result = new QuickSelector<int>(array).FindNth(9);
+
+            // assert
+            Assert.AreEqual(87, result, "item 87 is last and has index 9");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void QuickSelectForIndexOutOfRangeThrowsArgumentExceptionTest() {
+            // arrange
+            var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
+
+            // act
+            var result = new QuickSelector<int>(array).FindNth(10);
+        }
+
+    }
+
+
+}
---
+++ Arrays/QuickSelect/QuickSelector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSelect {
+
+    /// <summary>
+    /// Find the Nth element in an unordered array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QuickSelector<T> where T: IComparable {
+
+        private readonly T[] _array;
+
+        public T[] Array { 
+            get {
+                return _array;
+            }
+        }
+
+        public QuickSelector(T[] array) {
+            _array = array;
+        }
+
+        public T FindNth(int index) {
+            if(index < 0 || index >= Array.Length)
+                throw new ArgumentException("Searching index is out of range!");
+
+            return FindNth(0, Array.Length - 1, index);
+        }
+
+        private T FindNth(int start, int end, int index) {
+            Swap(start, index);
+
+            int i = start + 1;
+            int current = start;
+            while(i <= end) {
+                if(Array[start].CompareTo(Array[i]) > 0) {
+                    Swap(++current, i);
+                }
+                i++;
+            }
+
+            Swap(start, current);
+
+            switch(index.CompareTo(current)) {
+                case 1:
+                    return FindNth(current + 1, end, index);
+                case -1:
+                    return FindNth(start, current - 1, index);
+                default:
+                    return Array[current];
+            }
+        }
+
+        private void Swap(int index1, int index2) {
+            var swapping = Array[index1];
+            Array[index1] = Array[index2];
+            Array[index2] = swapping;
+        }
+
+    }
+
+}
