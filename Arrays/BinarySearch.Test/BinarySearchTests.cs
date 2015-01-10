b9a44fd28c7bd49a655500e92dbfe1cb42e6f150
Artem Tabalin <tabalinas@gmail.com>
2015-01-10 17:27:27 +0300

    Arrays: Binary search

---
+++ Arrays/BinarySearch.Test/BinarySearchTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinarySearch.Test {
+
+    [TestClass]
+    public class BinarySearchTests {
+
+        [TestMethod]
+        public void BinarySearchTest() {
+            // arrange
+            var array = new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 };
+
+            // act
+            int result = ArrayUtils.BinarySearch(array, -50);
+
+            // assert
+            Assert.AreEqual(1, result, "item -50 is at index 1");
+        }
+
+        [TestMethod]
+        public void BinarySearchOfLastItemTest() {
+            // arrange
+            var array = new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 };
+
+            // act
+            int result = ArrayUtils.BinarySearch(array, 87);
+
+            // assert
+            Assert.AreEqual(9, result, "item 87 is at index 9");
+        }
+
+        [TestMethod]
+        public void BinarySearchOfNonExistingItemTest() {
+            // arrange
+            var array = new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 };
+
+            // act
+            int result = ArrayUtils.BinarySearch(array, 100);
+
+            // assert
+            Assert.AreEqual(-1, result, "-1 is returned for non-existing item");
+        }
+
+    }
+
+
+}
---
+++ Arrays/BinarySearch/ArrayUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch {
+
+    public static class ArrayUtils {
+
+        public static int BinarySearch<T>(T[] array, T value) where T: IComparable {
+            var start = 0;
+            var end = array.Length - 1;
+
+            while(end >= start) {
+                int middle = (end - start) / 2 + start;
+
+                switch(array[middle].CompareTo(value)) {
+                    case -1:
+                        start = middle + 1;
+                        break;
+                    case 1:
+                        end = middle - 1;
+                        break;
+                    default:
+                        return middle;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+
+}
