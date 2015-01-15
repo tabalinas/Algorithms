e2b74b14edd9799094013b4382aeb84eba47ec20
Artem Tabalin <tabalinas@gmail.com>
2015-01-16 01:08:04 +0300

    Tree: Binary tree level printing Problem: Print binary tree levels as rows ending with newline sign

---
+++ Tree/BinaryTreeLevelPrinting.Test/TreeLevelPrintingTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BinarySearchTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryTreeLevelPrinting.Test {
+
+    [TestClass]
+    public class TreeLevelPrintingTests {
+
+        [TestMethod]
+        public void TreeLevelPrintingTest() {
+            // arrange
+            var tree = new BinarySearchTree<int>();
+            int[] elements = new int[] { 10, 7, 3, 9, 1, 5, 15, 13, 11, 14, 20, 17, 22 };
+            foreach (int el in elements) {
+                tree.Add(el);
+	        }
+
+            // act
+            string result = new TreePrinter<int>(tree).LevelPrint();
+
+            // assert
+            Assert.AreEqual(
+@"10
+7 15
+3 9 13 20
+1 5 11 14 17 22",
+                result
+            );
+        }
+
+    }
+
+}
---
+++ Tree/BinaryTreeLevelPrinting/TreePrinter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinarySearchTree;
+
+namespace BinaryTreeLevelPrinting {
+
+    /// <summary>
+    /// Print binary tree levels as rows ending with newline sign 
+    /// </summary>
+    public class TreePrinter<T> where T: IComparable {
+
+        private class NodeWithLevel {
+            public BinarySearchTree<T>.Node Node { get; set; }
+            public int Level { get; set; }
+
+            public NodeWithLevel(BinarySearchTree<T>.Node node, int level = 0) {
+                Node = node;
+                Level = level;
+            }
+        }
+
+        private readonly BinarySearchTree<T> _tree;
+
+        public TreePrinter(BinarySearchTree<T> tree) {
+            _tree = tree;
+        }
+
+        public string LevelPrint() {
+            var result = new StringBuilder();
+            var row = new StringBuilder();
+            
+            int level = 1;
+            var queue = new Queue<NodeWithLevel>();
+            queue.Enqueue(new NodeWithLevel(_tree.Root, level));
+
+            while(queue.Count > 0) {
+                var node = queue.Dequeue();
+                
+                if(node.Node == null)
+                    continue;
+
+                if(node.Level > level) {
+                    result.AppendLine(row.ToString());
+                    row = new StringBuilder();
+                    level = node.Level;
+                }
+
+                row.Append((row.Length > 0 ? " " : "") + node.Node.Value.ToString());
+
+                queue.Enqueue(new NodeWithLevel(node.Node.Left, level + 1));
+                queue.Enqueue(new NodeWithLevel(node.Node.Right, level + 1));
+            }
+
+            result.Append(row.ToString());
+
+            return result.ToString();
+        }
+
+        
+    }
+}
