1cd7a5c8b8a5c1a0c8d5f19612046fb4cd153a1b
Artem Tabalin <tabalinas@gmail.com>
2015-02-03 01:10:35 +0300

    Sorting: Counting sort

---
+++ Sorting/Sorting/CountingSorter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting {
+
+    public class CountingSorter: Sorter<int> {
+
+        public CountingSorter(int[] array)
+            : base(array) { }
+
+        public override void Sort() {
+            int min = Array.Min();
+            int max = Array.Max();
+
+            int[] result = new int[Array.Length];
+            int[] count = new int[max - min + 1];
+
+            foreach(int num in Array) {
+                count[num - min]++;
+            }
+
+            int k = 0;
+            for(int i = 0; i < count.Length; i++) {
+                for(int j = 0; j < count[i]; j++) {
+                    result[k++] = i + min;
+                }
+            }
+
+            result.CopyTo(Array, 0);
+        }
+
+    }
+
+}
--- Sorting/Sotring.Test/SortingTests.cs
+++ Sorting/Sotring.Test/SortingTests.cs
@@ -48,8 +48,6 @@ namespace Sotring.Test {
             // arrage
             var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
 
-            array = new int[] { 2, -86, -50, -4, 43, 62, 38, 87, 7, 68 };
-
             // act
             new QuickSorter<int>(array).Sort();
 
@@ -62,13 +60,23 @@ namespace Sotring.Test {
             // arrage
             var array = new int[] { 2, -86, 43, -50, 62, -4, 38, 87, 7, 68 };
 
-            array = new int[] { 2, -86, -50, -4, 43, 62, 38, 87, 7, 68 };
-
             // act
             new HeapSorter<int>(array).Sort();
 
             // assert
             CollectionAssert.AreEqual(new int[] { -86, -50, -4, 2, 7, 38, 43, 62, 68, 87 }, array);
+        }
+
+        [TestMethod]
+        public void CountingSortingTest() {
+            // arrange
+            var array = new int[] { 2, 3, 2, 5, 6, 1, 4, 6, 2, 5, 8, 9, 1, 4, 6, 4 };
+
+            // act
+            new CountingSorter(array).Sort();
+
+            // assert
+            CollectionAssert.AreEqual(new int[] { 1, 1, 2, 2, 2, 3, 4, 4, 4, 5, 5, 6, 6, 6, 8, 9 }, array);
         }
 
     }
