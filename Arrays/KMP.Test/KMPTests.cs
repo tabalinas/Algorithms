0358e2646fbd7d9deff9bfa400c484f36536a786
Artem Tabalin <tabalinas@gmail.com>
2015-02-08 21:20:33 +0100

    Arrays: Knuth-Morris-Pratt Substring Searching

---
+++ Arrays/KMP.Test/KMPTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KMP.Test {
+
+    [TestClass]
+    public class KMPTests {
+
+        [TestMethod]
+        public void KMPSearchTest() {
+            // arrange
+            string source = "ABC ABCDAB ABCDABCDABDE";
+            string searchValue = "ABCDABD";
+
+            // act
+            var result = new KMPSearch().StringPosition(source, searchValue);
+
+            // assert
+            Assert.AreEqual(15, result, "found at index 15");
+        }
+
+        [TestMethod]
+        public void KMPSearchFailureTest() {
+            // arrange
+            string source = "ABC ABCDAB ABCDABCDABDE";
+            string searchValue = "ABKCD";
+
+            // act
+            var result = new KMPSearch().StringPosition(source, searchValue);
+
+            // assert
+            Assert.AreEqual(-1, result, "string not found");
+        }
+
+    }
+}
---
+++ Arrays/KMP/KMPSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMP {
+
+    /// <summary>
+    /// Knuth-Morris-Pratt Substring Searching
+    /// </summary>
+    public class KMPSearch {
+
+        public int StringPosition(string str, string searchValue) {
+            int[] prefixTable = CreatePrefixTable(searchValue);
+
+            for(int i = 0, j = 0; i < str.Length; i++) {
+                while(j > 0 && str[i] != searchValue[j]) {
+                    j = prefixTable[j - 1];
+                }
+
+                if(str[i] == searchValue[j]) {
+                    j++;
+                }
+
+                if(j == searchValue.Length) {
+                    return i - j + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private int[] CreatePrefixTable(string str) {
+            int[] result = new int[str.Length];
+
+            for(int i = 1, j = 0; i < str.Length; i++) {
+                while(j > 0 && str[i] != str[j]) {
+                    j = result[j - 1];
+                }   
+
+                if(str[i] == str[j])
+                    j++;
+
+                result[i] = j;
+            }
+
+            return result;
+        }
+
+    }
+
+}
