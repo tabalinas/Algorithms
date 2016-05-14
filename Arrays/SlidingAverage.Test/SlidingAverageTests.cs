a2ec2c8640c2b441df414a434006a024c2692e8d
Artem Tabalin <tabalinas@gmail.com>
2016-05-15 00:52:49 +0200

    Arrays: Sliding Average

---
+++ Arrays/SlidingAverage.Test/SlidingAverageTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SlidingAverage.Test {
+    
+    [TestClass]
+    public class SlidingAverageTests {
+
+        [TestMethod]
+        public void SlidingAverageTest() {
+            // arrange
+            double[] source = new double[] { 3, 0, 3, 6, 1, 5, 4 };
+            int windowSize = 3;
+            double[] expectedResult = new double[] { 2, 3, 10/3.0, 4, 10/3.0, 4.5, 4 };
+
+            // act
+            double[] result = ArrayUtils.SlidingAverage(source, windowSize);
+
+            // assert
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+
+}
---
+++ Arrays/SlidingAverage/ArrayUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingAverage
+{
+    public class ArrayUtils
+    {
+        public static double[] SlidingAverage(double[] source, int windowSize) {
+            if(source == null || source.Length < windowSize)
+                throw new ArgumentException();
+
+            double[] result = new double[source.Length];
+
+            double sum = 0;
+            for(int i = 0; i < windowSize; i++) {
+                sum += source[i];
+            }
+
+            var windowFitLastIndex = source.Length - windowSize;
+
+            for(int i = 0; i < windowFitLastIndex; i++) {
+                result[i] = sum / windowSize;
+                sum += source[i + windowSize] - source[i];
+            }
+
+            for(int i = 0; i < windowSize; i++) {
+                result[windowFitLastIndex + i] = sum / (windowSize - i);
+                sum -= source[windowFitLastIndex + i];
+            }
+
+            return result;
+        }
+    }
+}
